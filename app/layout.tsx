ec20fd6a6fa19076402428cf0bf24b70813cbcdd
Ganeshkatam <katamganesh61@gmail.com>
2026-08-19 09:33:39 +0530

    fix(layout): remove unused material symbols stylesheet link to eliminate font warnings

--- app/layout.tsx
+++ app/layout.tsx
@@ -71,7 +71,6 @@ export default function RootLayout({
             __html: themeInitScript,
           }}
         />
-        <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200" />
       </head>
       <body className="antialiased font-sans" suppressHydrationWarning>
         <Providers>
