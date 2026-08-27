ac65e7c22d874e51bb30f9114ba1d0a21fe4db78
Ganeshkatam <katamganesh61@gmail.com>
2026-08-28 01:35:51 +0530

    refactor(landing): streamline layout to be purely literature-centric and eliminate marketing fluff

--- modules/landing/presentation/components/LandingClient.tsx
+++ modules/landing/presentation/components/LandingClient.tsx
@@ -5,13 +5,11 @@ import StatisticsSection from "./StatisticsSection";
 import FeaturedBooksSection from "./FeaturedBooksSection";
 import TrendingBooksSection from "./TrendingBooksSection";
 import RecentlyAddedSection from "./RecentlyAddedSection";
-import PlatformFeaturesSection from "./PlatformFeaturesSection";
 import FeaturedCollectionsSection from "./FeaturedCollectionsSection";
 import GenreBrowserSection from "./GenreBrowserSection";
 import SubjectsExplorerSection from "./SubjectsExplorerSection";
 import PopularAuthorsSection from "./PopularAuthorsSection";
 import AnnouncementSection from "./AnnouncementSection";
-import CallToActionSection from "./CallToActionSection";
 
 export interface LandingClientProps {
   model?: any;
@@ -40,32 +38,26 @@ export default function LandingClient({ model, overview }: LandingClientProps) {
       {/* 1. Grand Independent Hero */}
       <HeroSection searchSuggestions={featuredBooks} />
 
-      {/* 2. Platform Milestones & Statistics */}
+      {/* 2. Platform Catalog Scope & Metrics */}
       {statisticsData && <StatisticsSection statistics={statisticsData} />}
 
-      {/* 3. Primary Catalog Shelves & Browsers */}
-      <div className="w-full relative z-20 flex flex-col gap-20 pt-10 pb-16">
+      {/* 3. Pure Literary Catalog Shelves & Browsers */}
+      <div className="w-full relative z-20 flex flex-col gap-20 pt-8 pb-20">
         {/* Curated Catalog Shelves */}
         <FeaturedBooksSection items={featuredBooks} />
         <TrendingBooksSection items={trendingBooks} />
         <RecentlyAddedSection items={newBooks} />
 
-        {/* Platform Capabilities & Reader Experience */}
-        <PlatformFeaturesSection />
-
-        {/* Curated Collections */}
+        {/* Curated Literary Collections */}
         <FeaturedCollectionsSection collections={featuredCollections} />
 
-        {/* Discovery Taxonomies */}
+        {/* Discovery Disciplines & Taxonomies */}
         <GenreBrowserSection genres={genres} />
         <SubjectsExplorerSection subjects={subjects} />
         <PopularAuthorsSection authors={authors} />
 
-        {/* Platform Dispatches & Announcements */}
+        {/* Platform Dispatches & Notices */}
         <AnnouncementSection announcements={announcementsData} />
-
-        {/* Sanctuary Call To Action */}
-        <CallToActionSection />
       </div>
     </div>
   );
